93a4f003393024ff6f1684877b9c48b568901854
kheotor <heitorcanada99@gmail.com>
2026-07-04 21:43:11 -0300

    adição do exc034

--- exc034.py
+++ exc034.py
@@ -1,1 +1,11 @@
 print ("Programa que calcula aumento(se ganhar 1250+ é 10%.Se for menos que isso é 15%)")
+salario = int(input("Digite o seu salario: "))
+aumento10 = salario*10/100
+aumento15 = salario*15/100
+novosalario10 = salario+aumento10
+novosalario15 = salario+aumento15
+
+if salario >= 1250:
+    print (f"Com seus 10% de aumento você foi de {salario} para {novosalario10}")
+else:
+    print (f"Com seus 15% de aumento você foi de {salario} para {novosalario15}")
