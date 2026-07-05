857954f4ab2b0e8509af6bb2796c043e2763af04
kheotor <heitorcanada99@gmail.com>
2026-07-05 15:33:43 -0300

    PedraPapelTesourav1.1

--- ProjetoPedraPapelTesoura.py
+++ ProjetoPedraPapelTesoura.py
@@ -4,11 +4,7 @@ humanchoice = input("Digite sua jogada!: ")
 
 humanchoicelow = humanchoice.strip().lower()
 
-tesoura = ("tesoura")
-papel = ("papel")
-pedra = ("pedra")
-
-machine_lista = [tesoura,papel,pedra]
+machine_lista = ["tesoura","papel","pedra"]
 
 if humanchoicelow != machine_lista[0] and humanchoicelow != machine_lista[1] and humanchoicelow != machine_lista[2]:
     print("Ei isso não faz parte das escolhas,joga direito ai pow!")
@@ -16,27 +12,24 @@ if humanchoicelow != machine_lista[0] and humanchoicelow != machine_lista[1] and
 
 machinechoice = random.choice(machine_lista)
 
-print (f"Você escolheu {humanchoice} e o computador escolheu {machinechoice}")
-
 if humanchoicelow==machinechoice:
     print (f"Você escolheu {humanchoicelow} e ele támbem,empate.")
 
-
-if humanchoicelow == machine_lista[0] and machinechoice == machine_lista [1]:
+if humanchoicelow == "tesoura" and machinechoice == "papel":
     print ("Você escolheu tesoura e o robo papel KKKKKKK ganhasse")
 
-if humanchoicelow ==machine_lista[1] and machinechoice == machine_lista[2]:
+if humanchoicelow =="papel" and machinechoice == "pedra":
     print ("Você escolheu papel e o robo pedra KKKKKKK ganhasse")
 
-if humanchoicelow ==machine_lista[2] and machinechoice ==machine_lista[0]:
+if humanchoicelow =="pedra" and machinechoice =="tesoura":
     print ("Você escolheu pedra e o robo tesoura KKKKKKK ganhasse")
 
 
-if humanchoicelow == machine_lista[0] and machinechoice == machine_lista[2]:
+if humanchoicelow == "tesoura" and machinechoice == "pedra":
     print ("Perdesse newbaa,escolhece tesoura e ele pedra")
 
-if humanchoicelow ==machine_lista[1] and machinechoice == machine_lista[0]:
+if humanchoicelow =="papel" and machinechoice == "tesoura":
     print ("Perdesse newbaa,escolhece papel e ele tesoura")
 
-if humanchoicelow ==machine_lista[2] and machinechoice ==machine_lista[1]:
+if humanchoicelow =="pedra" and machinechoice =="papel":
     print("Perdesse newbaa,escolhece pedra e ele papel")
