163ec7d80360f40e94ecc8abdfc89ab02732ef6f
kheotor <heitorcanada99@gmail.com>
2026-07-07 16:36:34 -0300

    Adição do exc 038

---
+++ Atividades/exc038.py
@@ -0,0 +1,9 @@
+print ("Programa que compara 2 números:")
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+if numero1>numero2:
+    print ("O primeiro valor é maior.")
+elif numero2>numero1:
+    print ("O segundo valor é maior.")
+else:
+    print ("Os dois são iguais!")
