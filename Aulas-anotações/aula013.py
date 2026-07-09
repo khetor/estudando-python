a8e7b36e3899483942a5a0bd41472292f74e377f
kheotor <heitorcanada99@gmail.com>
2026-07-09 01:12:56 -0300

    Adição de aula13A e aula13B

---
+++ Aulas-anotações/aula013.py
@@ -0,0 +1,7 @@
+nome = ("Olá mundo")
+for c in range (0, 10):
+    print (nome)
+print ("FIM")
+numero = int(input("Digite um número: "))
+for c in range (0,numero+1):
+    print (c)
---
+++ Aulas-anotações/aula13b.py
@@ -0,0 +1,5 @@
+s = 0
+for i in range (0,4):
+    n = int(input("Digite um valor: "))
+    s+=n
+print (f"A soma dos valores é {s}")
