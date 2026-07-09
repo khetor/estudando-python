100fd503eeb55b6f5c3b01ab176f5ae3d9104446
kheotor <heitorcanada99@gmail.com>
2026-07-09 00:07:56 -0300

    Organizei e coloquei sons no PPT

--- Atividades/exc021.py
+++ Atividades/exc021.py
@@ -2,6 +2,6 @@ print ("Programa que toca som:")
 import pygame
 
 pygame.init()
-pygame.mixer.music.load("rizz-sound-effect.mp3")
+pygame.mixer.music.load("bob-esponja-fail-sound.mp3")
 pygame.mixer.music.play()
 input ("Aperte enter para finalizar.")
